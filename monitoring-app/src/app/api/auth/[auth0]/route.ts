6b808c6b46f68a5797c8581598f48f2b39ff67aa
Okko Ojala <okko.ojala@gmail.com>
2025-02-05 21:53:56 +0200

    Get user from db

--- monitoring-app/src/app/api/auth/[auth0]/route.ts
+++ monitoring-app/src/app/api/auth/[auth0]/route.ts
@@ -1,2 +1,13 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
-export const GET = handleAuth();
+import { handleAuth, handleCallback, Session } from "@auth0/nextjs-auth0";
+import { getUser } from "../../../../../db/db";
+const afterCallback = async (req: Request, session: Session) => {
+  const dbUser = await getUser(session.user.sub);
+  let user = { ...session.user };
+  if (dbUser) user.db = dbUser[0];
+  delete session.refreshToken;
+  return { ...session, user: user };
+};
+
+export const GET = handleAuth({
+  callback: handleCallback({ afterCallback }),
+});
