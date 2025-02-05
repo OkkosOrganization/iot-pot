be6f6fe7d80bed47404b4b684f9f8852dd95e6f2
Okko Ojala <okko.ojala@gmail.com>
2025-02-05 21:55:17 +0200

    use const

--- monitoring-app/src/app/api/auth/[auth0]/route.ts
+++ monitoring-app/src/app/api/auth/[auth0]/route.ts
@@ -2,7 +2,7 @@ import { handleAuth, handleCallback, Session } from "@auth0/nextjs-auth0";
 import { getUser } from "../../../../../db/db";
 const afterCallback = async (req: Request, session: Session) => {
   const dbUser = await getUser(session.user.sub);
-  let user = { ...session.user };
+  const user = { ...session.user };
   if (dbUser) user.db = dbUser[0];
   delete session.refreshToken;
   return { ...session, user: user };
