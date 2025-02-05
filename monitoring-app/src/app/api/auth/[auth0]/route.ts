d875daffa0e2ca25c1901a41be16d25f94d5dd41
Okko Ojala <okko.ojala@gmail.com>
2025-02-05 22:46:39 +0200

    Create user in db if not exists

--- monitoring-app/src/app/api/auth/[auth0]/route.ts
+++ monitoring-app/src/app/api/auth/[auth0]/route.ts
@@ -1,9 +1,15 @@
 import { handleAuth, handleCallback, Session } from "@auth0/nextjs-auth0";
-import { getUser } from "../../../../../db/db";
+import { addUser, getUser } from "../../../../../db/db";
 const afterCallback = async (req: Request, session: Session) => {
   const dbUser = await getUser(session.user.sub);
   const user = { ...session.user };
-  if (dbUser) user.db = dbUser[0];
+  console.log(dbUser);
+  if (dbUser) user.db = dbUser;
+  else {
+    const newUser = await addUser(session.user.sub, session.user.email);
+    user.db = newUser;
+  }
+
   delete session.refreshToken;
   return { ...session, user: user };
 };
