209ac0b7c3a4c5d3ad60c66802f104edc710d03c
Okko Ojala <okko.ojala@gmail.com>
2025-02-05 23:15:59 +0200

    Remove unused import

--- monitoring-app/src/app/dashboard/page.tsx
+++ monitoring-app/src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { UserInfo } from "../components/UserInfo";
 import { LogoutButton } from "../components/LogoutButton";
