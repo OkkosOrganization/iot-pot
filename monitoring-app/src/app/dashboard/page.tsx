cf24e3054379ee0a63ad1bb1ab2b709da4ff7294
Okko Ojala <okko.ojala@gmail.com>
2025-02-05 23:15:07 +0200

    Use withPageAuthRequired

--- monitoring-app/src/app/dashboard/page.tsx
+++ monitoring-app/src/app/dashboard/page.tsx
@@ -1,16 +1,18 @@
-import { getSession } from "@auth0/nextjs-auth0";
-import Link from "next/link";
-import { redirect } from "next/navigation";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { UserInfo } from "../components/UserInfo";
+import { LogoutButton } from "../components/LogoutButton";
 
-export default async function DashBoardPage() {
-  const session = await getSession();
-  if (session?.user) {
+export default withPageAuthRequired(
+  async function DashBoardPage() {
     return (
-      <div>
-        <h1>Welcome {session.user.name}</h1>
-        <Link href="/api/auth/logout">LOGOUT</Link>
-        <pre>{JSON.stringify(session, null, 5)}</pre>
-      </div>
+      <UserProvider>
+        <div>
+          <LogoutButton />
+          <UserInfo />
+        </div>
+      </UserProvider>
     );
-  } else return redirect("/");
-}
+  },
+  { returnTo: "/" }
+);
