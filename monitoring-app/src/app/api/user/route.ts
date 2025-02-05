70765d3af9f6bbba3fe71d7f1605386ca440102d
Okko Ojala <okko.ojala@gmail.com>
2025-02-05 18:31:26 +0200

    Add code for adding user to DB

--- monitoring-app/src/app/api/user/route.ts
+++ monitoring-app/src/app/api/user/route.ts
@@ -1,11 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
+import { db } from "../../../../db/db";
+import { users } from "../../../../drizzle/schema";
 
 export async function POST(request: NextRequest) {
   const { id, email } = await request.json();
   console.log(id, email);
-  const greeting = "Hello user: " + id + ", email: " + email;
-  const json = {
-    greeting,
-  };
-  return NextResponse.json(json);
+
+  if (!id) return NextResponse.json({ success: 0, error: "No auth0 id" });
+
+  try {
+    const rowId = await db
+      .insert(users)
+      .values({
+        auth0Id: id,
+        email: email,
+      })
+      .returning({ dbId: users.id });
+    return NextResponse.json({ success: 1, data: rowId });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ success: 0, error: e });
+  }
 }
