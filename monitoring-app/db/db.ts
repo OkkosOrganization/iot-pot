20963756e322c9c3576a9a5d8f9ee760781f3a97
Okko Ojala <okko.ojala@gmail.com>
2025-02-05 22:46:14 +0200

    get users devices

--- monitoring-app/db/db.ts
+++ monitoring-app/db/db.ts
@@ -3,19 +3,19 @@ import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "../drizzle/schema";
 export const db = drizzle(process.env.DATABASE_URL!, { schema: schema });
 import { eq, desc } from "drizzle-orm";
+import { authUid } from "drizzle-orm/neon";
 
 export const getUser = async (userAuth0Id: string) => {
   const dbData = await db
     .select()
     .from(schema.users)
-    .where(eq(schema.users.auth0Id, userAuth0Id));
-  //.leftJoin(schema.devices, eq(schema.users.id, schema.devices.userId));
-  return dbData;
-  /*
-  console.log(userAuth0Id);
-  if (dbData) {
-    const devices = dbData.map((d) => {
-      return d.devices;
+    .where(eq(schema.users.auth0Id, userAuth0Id))
+    .leftJoin(schema.devices, eq(schema.users.id, schema.devices.userId));
+
+  console.log(dbData);
+  if (dbData.length) {
+    const devices = dbData.filter((d) => {
+      d.devices !== null ? d.devices : false;
     });
 
     const userWithDevices = {
@@ -24,7 +24,6 @@ export const getUser = async (userAuth0Id: string) => {
     };
     return userWithDevices;
   }
-  */
 
   return null;
 };
@@ -36,4 +35,14 @@ export const getLatestMeasurements = (deviceId: number) => {
     .where(eq(schema.measurements.deviceId, deviceId))
     .orderBy(desc(schema.measurements.id))
     .limit(1);
+};
+
+export const addUser = (auth0Id: string, email: string) => {
+  return db
+    .insert(schema.users)
+    .values({
+      auth0Id: auth0Id,
+      email: email,
+    })
+    .returning();
 };
