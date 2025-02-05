51375f1fc5a5dbbfee8d2a811d27c6135b5cfa77
Okko Ojala <okko.ojala@gmail.com>
2025-02-05 21:53:21 +0200

    Add logout link

--- monitoring-app/src/app/dashboard/page.tsx
+++ monitoring-app/src/app/dashboard/page.tsx
@@ -4,6 +4,12 @@ import { redirect } from "next/navigation";
 export default async function DashBoardPage() {
   const session = await getSession();
   if (session?.user) {
-    return <h1>Welcome {session.user.name}</h1>;
+    return (
+      <div>
+        <h1>Welcome {session.user.name}</h1>
+        <a href="/api/auth/logout">LOGOUT</a>
+        <pre>{JSON.stringify(session, null, 5)}</pre>
+      </div>
+    );
   } else return redirect("/");
 }
