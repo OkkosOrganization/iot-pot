7cbb488a9c10e8cea8fbddd97e1f207eaa472758
Okko Ojala <okko.ojala@gmail.com>
2025-02-06 23:51:18 +0200

    Add layout

---
+++ monitoring-app/src/app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import { getSession } from "@auth0/nextjs-auth0";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { redirect } from "next/navigation";
+import { Navi } from "../components/Navi";
+import styles from "./layout.module.css";
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getSession();
+  if (!session) return redirect("/");
+  return (
+    <UserProvider>
+      <div className={styles.dashboard}>
+        <Navi />
+        <section>{children}</section>;
+      </div>
+    </UserProvider>
+  );
+}
