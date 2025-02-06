7570f11df45602a20e3ee83d79d5a60b92e51f22
Okko Ojala <okko.ojala@gmail.com>
2025-02-06 23:52:18 +0200

    Move stuff to layout

--- monitoring-app/src/app/dashboard/page.tsx
+++ monitoring-app/src/app/dashboard/page.tsx
@@ -1,21 +1,5 @@
-import { getSession } from "@auth0/nextjs-auth0";
-import { redirect } from "next/navigation";
-import { LogoutButton } from "../components/LogoutButton";
 import { UserInfo } from "../components/UserInfo";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
 
-export default async function DashBoardPage() {
-  const session = await getSession();
-  if (session?.user) {
-    return (
-      <div>
-        <UserProvider>
-          <div>
-            <LogoutButton />
-            <UserInfo />
-          </div>
-        </UserProvider>
-      </div>
-    );
-  } else return redirect("/");
+export default async function DashboardPage() {
+  return <UserInfo />;
 }
