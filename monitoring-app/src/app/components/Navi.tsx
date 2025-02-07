fce2562ccbc0e2e11d2fce95069725ca871a0765
Okko Ojala <okko.ojala@gmail.com>
2025-02-07 16:11:12 +0200

    Add device navi

--- monitoring-app/src/app/components/Navi.tsx
+++ monitoring-app/src/app/components/Navi.tsx
@@ -4,6 +4,11 @@ import { Logo } from "./Logo";
 import { LogoutButton } from "./LogoutButton";
 import styles from "./Navi.module.css";
 import Link from "next/link";
+import { PlantIcon } from "./PlantIcon";
+import { usePathname } from "next/navigation";
+import { PlusIcon } from "./PlusIcon";
+import { useState } from "react";
+import { AddDeviceDialog } from "./AddDeviceDialog";
 type Device = {
   id: number;
   deviceId: string;
@@ -20,26 +25,49 @@ type UserWithDb = {
 
 type User = UserProfile & UserWithDb;
 export const Navi = () => {
+  const [showDialog, setShowDialog] = useState<boolean>(false);
   const { user } = useUser();
+  const pathname = usePathname();
   const u = user as User;
   return (
-    <nav className={styles.navi}>
-      <Logo />
-      <ul className={styles.deviceNavi}>
-        {u?.db.devices.map((d, i) => {
-          return (
-            <li key={`deviceMenuItem_${i}`} className={styles.deviceNaviItem}>
-              <Link
-                href={`/dashboard/device/${d.deviceId}`}
-                className={styles.deviceNaviItemLink}
+    <>
+      <nav className={styles.navi}>
+        <Logo />
+        <ul className={styles.deviceNavi}>
+          {u?.db.devices.map((d, i) => {
+            const isActiveItem = pathname.includes(
+              `/dashboard/device/${d.deviceId}`
+            );
+
+            return (
+              <li
+                key={`deviceMenuItem_${i}`}
+                className={`${styles.deviceNaviItem} ${
+                  isActiveItem ? styles.active : ""
+                }`}
               >
-                {d.title}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-      <LogoutButton />
-    </nav>
+                <Link
+                  href={`/dashboard/device/${d.deviceId}`}
+                  className={`${styles.deviceNaviItemLink} `}
+                >
+                  <div className={`${styles.naviItemIcon} `}>
+                    <PlantIcon />
+                  </div>
+                  <span className={styles.naviItemTitle}>{d.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+          <li
+            className={styles.addDeviceBtn}
+            onClick={() => setShowDialog(true)}
+          >
+            <PlusIcon />
+          </li>
+        </ul>
+        <LogoutButton />
+      </nav>
+      <AddDeviceDialog showDialog={showDialog} setShowDialog={setShowDialog} />
+    </>
   );
 };
