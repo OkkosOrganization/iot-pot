e151499ddc746e513a1f6cb43c6be93714d9c4fc
Okko Ojala <okko.ojala@gmail.com>
2025-02-07 21:24:19 +0200

    Add X icon

---
+++ monitoring-app/src/app/components/xicon.tsx
@@ -0,0 +1,9 @@
+export const Xicon = () => (
+  <svg width="10" height="10" viewBox="0 0 10 10" fill="none">
+    <path
+      d="M1.43507 8.50608L4.9706 4.97055M8.50614 1.43502L4.9706 4.97055M4.9706 4.97055L1.43507 1.43502L8.50614 8.50608"
+      stroke="#4A4A4A"
+      strokeWidth="1.5"
+    />
+  </svg>
+);
