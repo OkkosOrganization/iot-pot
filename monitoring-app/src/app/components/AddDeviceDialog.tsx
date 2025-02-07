944968a19f24702044ac37c2defa9907d8ff5d92
Okko Ojala <okko.ojala@gmail.com>
2025-02-07 21:23:35 +0200

    Add Dialog

--- monitoring-app/src/app/components/AddDeviceDialog.tsx
+++ monitoring-app/src/app/components/AddDeviceDialog.tsx
@@ -1,23 +1,86 @@
-import { Dispatch, SetStateAction } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useActionState,
+  useEffect,
+  useRef,
+} from "react";
 import styles from "./AddDeviceDialog.module.css";
+import { AddDevice } from "./actions";
+import { Device } from "./Navi";
+import { Xicon } from "./xicon";
+
 type AddDeviceDialogProps = {
   showDialog: boolean;
   setShowDialog: Dispatch<SetStateAction<boolean>>;
+  userId: number;
+  setDevices: Dispatch<SetStateAction<Device[]>>;
 };
+type State = {
+  device: Device[];
+  error: null | string;
+};
+const initialState = {
+  device: [],
+  error: null,
+};
+
 export const AddDeviceDialog = ({
   showDialog,
   setShowDialog,
+  setDevices,
 }: AddDeviceDialogProps) => {
+  const [state, formAction] = useActionState(AddDevice, initialState);
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
+  useEffect(() => {
+    if (state?.device?.length) {
+      console.log("Add new device");
+      setDevices((prev) => {
+        const updatedDevices = [...prev].concat(
+          state.device as unknown as Device
+        );
+        return updatedDevices;
+      });
+    }
+  }, [state?.device, setDevices]);
   return (
-    <dialog open={showDialog} className={styles.dialog}>
+    <dialog open={showDialog} className={styles.dialog} ref={dialogRef}>
       <div className={styles.container}>
-        <p>Lisää laite</p>
-        <button
-          className={styles.closeDialogBtn}
-          onClick={() => setShowDialog(false)}
-        >
-          X
-        </button>
+        <header className={styles.header}>
+          <h2 className={styles.title}>Add new device</h2>
+          <button
+            className={styles.closeDialogBtn}
+            onClick={() => {
+              dialogRef.current?.close();
+              setShowDialog(false);
+            }}
+          >
+            <Xicon />
+          </button>
+        </header>
+        <form action={formAction} className={styles.form}>
+          <div className={styles.inputContainer}>
+            <label className={styles.inputLabel}>Device ID</label>
+            <input
+              type="text"
+              name="deviceId"
+              className={styles.inputText}
+              required
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label className={styles.inputLabel}>Title</label>
+            <input
+              type="text"
+              name="title"
+              className={styles.inputText}
+              required
+            />
+          </div>
+          <button type="submit" className={styles.submitBtn}>
+            ADD DEVICE
+          </button>
+        </form>
       </div>
     </dialog>
   );
