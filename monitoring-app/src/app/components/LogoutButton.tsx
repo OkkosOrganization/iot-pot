b42ccf1e7cb8e8e5c6e67efc744e9904d5ec79e5
Okko Ojala <okko.ojala@gmail.com>
2025-02-07 21:36:39 +0200

    Use Link

--- monitoring-app/src/app/components/LogoutButton.tsx
+++ monitoring-app/src/app/components/LogoutButton.tsx
@@ -3,7 +3,9 @@ import styles from "./LogoutButton.module.css";
 export const LogoutButton = () => {
   return (
     <button className={styles.logoutBtn}>
-      <Link href="/api/auth/logout">LOGOUT</Link>
+      <Link href="/api/auth/logout" prefetch={false}>
+        LOGOUT
+      </Link>
     </button>
   );
 };
