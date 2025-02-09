dc5211e92ed46e4110deb6767414ea6a9056302e
Okko Ojala <okko.ojala@gmail.com>
2025-02-09 20:00:49 +0200

    Add icons

---
+++ monitoring-app/src/app/components/LogoutIcon.tsx
@@ -0,0 +1,18 @@
+export const LogoutIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <mask id="path-1-inside-1_16_387" fill="white">
+      <path d="M12 20C7.58172 20 4 16.4183 4 12C4 7.58172 7.58172 4 12 4" />
+    </mask>
+    <path
+      d="M12 19C8.134 19 5 15.866 5 12H3C3 16.9706 7.02944 21 12 21V19ZM5 12C5 8.134 8.134 5 12 5V3C7.02944 3 3 7.02944 3 12H5Z"
+      fill="#727272"
+      fillOpacity="0.75"
+      mask="url(#path-1-inside-1_16_387)"
+    />
+    <path
+      d="M10 12H20M20 12L17 9M20 12L17 15"
+      stroke="#727272"
+      strokeOpacity="0.75"
+    />
+  </svg>
+);
