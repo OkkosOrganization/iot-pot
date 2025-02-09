51284c6600ab0d277b6a0ee81b58e0fba4b2f5bc
Okko Ojala <okko.ojala@gmail.com>
2025-02-09 16:23:29 +0200

    Fix types

--- monitoring-app/src/app/page.tsx
+++ monitoring-app/src/app/page.tsx
@@ -1,14 +1,14 @@
 import styles from "./page.module.css";
 import { LoginButton } from "./components/LoginButton";
 import { Logo } from "./components/Logo";
-import { SearchParams } from "next/dist/server/request/search-params";
-
-export default async function Home({
-  searchParams,
-}: {
+type SP = {
   searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  const sp = (await searchParams) as SearchParams;
+};
+type PageProps = {
+  searchParams: Promise<SP>;
+};
+export default async function Home({ searchParams }: PageProps) {
+  const sp = await searchParams;
   const emailNotVerified = "email-not-verified" in sp;
   return (
     <div className={styles.page}>
