fd2a315604de4a7589e80dc2ea22d854feaf8ce5
Okko Ojala <okko.ojala@gmail.com>
2025-02-11 14:46:17 +0200

    Add hover fx

--- monitoring-app/src/app/components/icons/LogoutIcon.tsx
+++ monitoring-app/src/app/components/icons/LogoutIcon.tsx
@@ -5,14 +5,10 @@ export const LogoutIcon = () => (
     </mask>
     <path
       d="M12 19C8.134 19 5 15.866 5 12H3C3 16.9706 7.02944 21 12 21V19ZM5 12C5 8.134 8.134 5 12 5V3C7.02944 3 3 7.02944 3 12H5Z"
-      fill="#727272"
+      fill="currentColor"
       fillOpacity="0.75"
       mask="url(#path-1-inside-1_16_387)"
     />
-    <path
-      d="M10 12H20M20 12L17 9M20 12L17 15"
-      stroke="#727272"
-      strokeOpacity="0.75"
-    />
+    <path d="M10 12H20M20 12L17 9M20 12L17 15" stroke="currentColor" />
   </svg>
 );
