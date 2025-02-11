34b7e5d7441b7767239b3dbaca3a599627559354
Okko Ojala <okko.ojala@gmail.com>
2025-02-11 11:55:08 +0200

    Add SensorCard

---
+++ monitoring-app/src/app/components/SensorCard.tsx
@@ -0,0 +1,54 @@
+import { AirHumidityIcon } from "./icons/AirHumidity";
+import { AirTemperatureIcon } from "./icons/AirTemperature";
+import { LuminosityIcon } from "./icons/LuminosityIcon";
+import { WaterOverFlowIcon } from "./icons/WaterOverFlowIcon";
+import { SoilMoistureIcon } from "./icons/SoilMoistureIcon";
+import { WaterLevelIcon } from "./icons/WaterLevelIcon";
+import styles from "./SensorCard.module.css";
+type SensorTypes =
+  | "soilMoisture"
+  | "soilTemperature"
+  | "airTemperature"
+  | "airHumidity"
+  | "luminosity"
+  | "waterLevel"
+  | "waterOverflow";
+type SensorCardProps = {
+  type: SensorTypes;
+  title: string;
+  value: number;
+  unit: string;
+};
+export const SensorCard = ({ type, title, value, unit }: SensorCardProps) => {
+  return (
+    <div className={`${styles.container}`}>
+      <div className={styles.content}>
+        <h3 className={styles.title}>{title}</h3>
+        <div className={styles.iconContainer}>
+          {(() => {
+            switch (type) {
+              case "soilMoisture":
+                return <SoilMoistureIcon />;
+              case "waterLevel":
+                return <WaterLevelIcon />;
+              case "luminosity":
+                return <LuminosityIcon />;
+              case "airTemperature":
+                return <AirTemperatureIcon />;
+              case "airHumidity":
+                return <AirHumidityIcon />;
+              case "waterOverflow":
+                return <WaterOverFlowIcon />;
+              default:
+                return null;
+            }
+          })()}
+        </div>
+        <h4 className={styles.valueAndUnit}>
+          <span className={styles.value}>{value}</span>
+          <span className={styles.unit}>{unit}</span>
+        </h4>
+      </div>
+    </div>
+  );
+};
