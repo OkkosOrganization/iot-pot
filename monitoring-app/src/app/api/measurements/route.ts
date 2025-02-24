8953720afdb951570c27027a6ee0fcfa71584591
Okko Ojala <okko.ojala@gmail.com>
2025-02-24 22:01:30 +0200

    Check that device exists and is linked to user

--- monitoring-app/src/app/api/measurements/route.ts
+++ monitoring-app/src/app/api/measurements/route.ts
@@ -1,16 +1,28 @@
 import { NextResponse } from "next/server";
 import { type NextRequest } from "next/server";
-import { addMeasurements } from "../../../../db/db";
+import { addMeasurements, getDevice } from "../../../../db/db";
 import { SensorValues } from "@/app/contexts/mqttContext";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const { deviceId, sensorValues } = body;
   if (deviceId && sensorValues) {
-    console.log(deviceId, sensorValues);
     try {
-      await addMeasurements(deviceId, sensorValues as SensorValues);
-      return NextResponse.json({ success: 1 });
+      const devices = await getDevice(deviceId);
+      if (devices.length) {
+        if (devices[0].userId !== null) {
+          await addMeasurements(deviceId, sensorValues as SensorValues);
+          return NextResponse.json({ success: 1 });
+        } else
+          return NextResponse.json(
+            { success: 0, error: "No user for device" },
+            { status: 400 }
+          );
+      } else
+        return NextResponse.json(
+          { success: 0, error: "No device found" },
+          { status: 400 }
+        );
     } catch (e) {
       return NextResponse.json({ success: 0, error: e }, { status: 400 });
     }
