0f7ae5ae53f016a5df5cde6f92c43284fdc4b069
Okko Ojala <okko.ojala@gmail.com>
2025-02-25 22:33:07 +0200

    Rename loop function, use global variables for measurements, remove Fahrenheit

--- include/dht22_sensor.h
+++ include/dht22_sensor.h
@@ -8,37 +8,34 @@
 #define DHT_SENSOR_TYPE DHT22
 
 DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+float airHumidity = 0;
+float airTemperature = 0;
+
+void getAirTemperatureAndHumidity();
 
 void setup() {
   Serial.begin(9600);
   dht_sensor.begin(); // initialize the DHT sensor
 }
 
-void loop() {
+void getAirTemperatureAndHumidity() {
   // read humidity
   float humi  = dht_sensor.readHumidity();
   // read temperature in Celsius
   float temperature_C = dht_sensor.readTemperature();
-  // read temperature in Fahrenheit
-  float temperature_F = dht_sensor.readTemperature(true);
 
   // check whether the reading is successful or not
-  if ( isnan(temperature_C) || isnan(temperature_F) || isnan(humi)) {
+  if ( isnan(temperature_C) || isnan(humi)) {
     Serial.println("Failed to read from DHT sensor!");
   } else {
     Serial.print("Humidity: ");
     Serial.print(humi);
     Serial.print("%");
-
     Serial.print("  |  ");
-
     Serial.print("Temperature: ");
     Serial.print(temperature_C);
     Serial.print("°C  ~  ");
-    Serial.print(temperature_F);
-    Serial.println("°F");
+    airHumidity = humi;
+    airTemperature = temperature_C;
   }
-
-  // wait a 2 seconds between readings
-  delay(2000);
 }
