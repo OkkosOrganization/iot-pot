209d68b111d1c9b34e94aee131e35e87461ffa2a
Okko Ojala <okko.ojala@gmail.com>
2025-03-06 20:40:30 +0200

    Add API route for unlinking device

---
+++ monitoring-app/src/app/api/device/unlink/[id]/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from "next/server";
+import { unLinkDevice } from "../../../../../../db/db";
+import { getSession, updateSession } from "@auth0/nextjs-auth0";
+import { Device } from "@/app/components/Navi";
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const session = await getSession();
+  const _params = await params;
+  if (!session?.user) {
+    return NextResponse.json(
+      { success: 0, error: "Not authorized" },
+      { status: 400 }
+    );
+  }
+
+  if (!_params.id) {
+    return NextResponse.json(
+      { success: 0, error: "No deviceId" },
+      { status: 400 }
+    );
+  }
+
+  if (
+    !session.user.db.devices.filter((d: Device) => d.deviceId === _params.id)
+      .length
+  )
+    return NextResponse.json(
+      { success: 0, error: "Not device owner" },
+      { status: 400 }
+    );
+
+  try {
+    await unLinkDevice(_params.id);
+    const newSession = { ...session };
+    newSession.user.db.devices = newSession?.user.db.devices.filter(
+      (d: Device) => d.deviceId !== _params.id
+    );
+    await updateSession({ ...newSession });
+    return NextResponse.json({ success: 1 });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ success: 0, error: e }, { status: 400 });
+  }
+}
