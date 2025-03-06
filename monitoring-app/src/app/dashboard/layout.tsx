1eeff6525bae1db29542a3f79e1c6804adab5c20
Okko Ojala <okko.ojala@gmail.com>
2025-03-06 20:39:19 +0200

    Add extended user context

--- monitoring-app/src/app/dashboard/layout.tsx
+++ monitoring-app/src/app/dashboard/layout.tsx
@@ -2,7 +2,9 @@ import { getSession } from "@auth0/nextjs-auth0";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { redirect } from "next/navigation";
 import { Navi, User } from "../components/Navi";
+import { ExtendedUserProvider } from "../contexts/extendedUserContext";
 import styles from "./layout.module.css";
+
 export default async function DashboardLayout({
   children,
 }: {
@@ -12,10 +14,12 @@ export default async function DashboardLayout({
   if (!session) return redirect("/");
   return (
     <UserProvider>
-      <div className={styles.dashboard}>
-        <Navi user={session?.user as User} />
-        <section className={styles.pageContainer}>{children}</section>
-      </div>
+      <ExtendedUserProvider>
+        <div className={styles.dashboard}>
+          <Navi user={session?.user as User} />
+          <section className={styles.pageContainer}>{children}</section>
+        </div>
+      </ExtendedUserProvider>
     </UserProvider>
   );
 }
