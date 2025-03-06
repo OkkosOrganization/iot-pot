5c6d40b499932a4c4fec376cdfc4fcdc7f0f0347
Okko Ojala <okko.ojala@gmail.com>
2025-03-06 20:41:01 +0200

    Update settings page

--- monitoring-app/src/app/dashboard/settings/page.tsx
+++ monitoring-app/src/app/dashboard/settings/page.tsx
@@ -1,10 +1,5 @@
 import { UserInfo } from "@/app/components/UserInfo";
 
 export default async function SettingsPage() {
-  return (
-    <div>
-      <h2>Settings / User info</h2>
-      <UserInfo />
-    </div>
-  );
+  return <UserInfo />;
 }
