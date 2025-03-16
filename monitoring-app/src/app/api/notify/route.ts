99a5399fd278cd4523321c3f0e1122061e11aa03
Okko Ojala <okko.ojala@gmail.com>
2025-03-16 13:15:08 +0200

    use mailgun package

--- monitoring-app/src/app/api/notify/route.ts
+++ monitoring-app/src/app/api/notify/route.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
+import Mailgun from "mailgun.js";
 import { getDevice } from "../../../../db/db";
 import { isValidEmail } from "@/utils";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const { deviceId, email } = body;
+  const { deviceId, email, msg } = body;
 
   // CHECK PARAMS
   if (!deviceId || !email) {
@@ -18,6 +19,13 @@ export async function POST(request: NextRequest) {
   if (!isValidEmail(email))
     return NextResponse.json(
       { success: 0, error: "Invalid email" },
+      { status: 400 }
+    );
+
+  // CHECK API KEY
+  if (!process.env.MAILGUN_API_KEY)
+    return NextResponse.json(
+      { success: 0, error: "Invalid API-key" },
       { status: 400 }
     );
 
@@ -37,6 +45,23 @@ export async function POST(request: NextRequest) {
       );
     else {
       // CALL MAILGUN API
+      const mailgun = new Mailgun(FormData);
+      const mg = mailgun.client({
+        username: "api",
+        key: process.env.MAILGUN_API_KEY as string,
+        url: "https://api.eu.mailgun.net",
+      });
+      //const msg = `A notification for you from device: ${deviceId}`;
+      const msgHtml = `<h1>${msg}</h1>`;
+      const res = await mg.messages.create("mail.iot-pot.com", {
+        from: "No-Reply <noreply@mail.iot-pot.com>",
+        to: [email],
+        subject: "IoT-Pot notification",
+        text: msg,
+        html: msgHtml,
+        url: "https://api.eu.mailgun.net",
+      });
+      console.log(res);
       return NextResponse.json({ success: 1 });
     }
   } catch (e) {
