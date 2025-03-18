2d7faa15ae8f99715af47d2a05b413e4cbfb1922
Okko Ojala <okko.ojala@gmail.com>
2025-03-18 20:55:56 +0200

    Update globals, add LED pin definitions

--- include/globals.h
+++ include/globals.h
@@ -9,3 +9,13 @@ float solPh = 0;
 float soilMoisture = 0;
 float soilTemperature = 0;
 int overflowValue = 0;
+
+// LEDS
+uint8_t LED_PIN_1 = D11;
+uint8_t LED_PIN_2 = D12;
+uint8_t LED_PIN_3 = D10;
+uint8_t LED_PIN_4 = D9;
+uint8_t LED_PIN_5 = D8;
+uint8_t LED_PIN_6 = D5;
+uint8_t LED_PIN_7 = D4;
+uint8_t LED_PIN_8 = D3;
