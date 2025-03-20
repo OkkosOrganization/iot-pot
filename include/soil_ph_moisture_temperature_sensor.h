a9159a7fa8f031cea3bf061c5a94d7a0636b5458
Okko Ojala <okko.ojala@gmail.com>
2025-03-20 17:28:12 +0200

    fix typo

--- include/soil_ph_moisture_temperature_sensor.h
+++ include/soil_ph_moisture_temperature_sensor.h
@@ -12,29 +12,25 @@ const int rxPin = 7; // RX (to RS485 TX)
 void initSoilSensor();
 void getSoilSensorValues();
 
-unsigned long lastSoilMeasurementTime = 0;
-const unsigned long soilMeasurementInterval = 5000; // 5 seconds
 
 void initSoilSensor() {
     Serial.begin(115200);  // Serial Monitor
     SensorSerial.begin(9600, SERIAL_8N1, rxPin, txPin); // Sensor default baud rate = 4800
 
     node.begin(1, SensorSerial); // Slave ID = 1
-    Serial.println("Modbus communication initialized...");
+    // Serial.println("Modbus communication initialized...");
 }
 
 void getSoilSensorValues() {
 
-  if (millis() - lastSoilMeasurementTime >= soilMeasurementInterval) {
-    lastSoilMeasurementTime = millis(); // Update timestamp
     uint8_t result;
 
     // Read Soil pH (Register 0x0003)
     result = node.readHoldingRegisters(0x0003, 1);
     if (result == node.ku8MBSuccess) {
-        solPh = node.getResponseBuffer(0);
-        Serial.print("Soil pH: ");
-        Serial.println(solPh, 2);
+        soilPh = node.getResponseBuffer(0);
+        // Serial.print("Soil pH: ");
+        // Serial.println(soilPh, 2);
     } else {
         Serial.print("Error reading pH, Code: ");
         Serial.println(result);
@@ -44,8 +40,8 @@ void getSoilSensorValues() {
     result = node.readHoldingRegisters(0x0000, 1);
     if (result == node.ku8MBSuccess) {
         soilMoisture = node.getResponseBuffer(0); // 10.0; // Convert to %
-        Serial.print("Soil Moisture: ");
-        Serial.println(soilMoisture, 1);
+        // Serial.print("Soil Moisture: ");
+        // Serial.println(soilMoisture, 1);
     } else {
         Serial.print("Error reading Moisture, Code: ");
         Serial.println(result);
@@ -55,11 +51,11 @@ void getSoilSensorValues() {
     result = node.readHoldingRegisters(0x0001, 1);
     if (result == node.ku8MBSuccess) {
         soilTemperature = node.getResponseBuffer(0) / 10.0; // Convert to °C
-        Serial.print("Soil Temperature: ");
-        Serial.println(soilTemperature, 1);
+        // Serial.print("Soil Temperature: ");
+        // Serial.println(soilTemperature, 1);
     } else {
         Serial.print("Error reading Temperature, Code: ");
         Serial.println(result);
     }
-  }
+  
 }
