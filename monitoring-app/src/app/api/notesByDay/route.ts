4a39c210565d83352273f249e77f61db2cca5e5d
Okko Ojala <okko.ojala@gmail.com>
2025-03-26 00:25:15 +0200

    Add routes

---
+++ monitoring-app/src/app/api/notesByDay/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { getNotesByDay, getNotesByWeek } from "../../../../db/db";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const deviceId = searchParams.get("deviceId");
+  const date = searchParams.get("date");
+  if (deviceId && date) {
+    try {
+      const data = await getNotesByDay(deviceId as string, date);
+      return NextResponse.json({ success: 1, data: data });
+    } catch (e) {
+      console.log(e);
+      return NextResponse.json({ success: 0, error: e });
+    }
+  }
+  return NextResponse.json({ success: 0, error: "Invalid params" });
+}
