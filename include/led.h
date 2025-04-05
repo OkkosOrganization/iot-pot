841f93982a420bd6597fb3b72fc6b6d5d4f09fc8
Okko Ojala <okko.ojala@gmail.com>
2025-04-05 17:49:52 +0300

    Add comments that tell the use of the leds

--- include/led.h
+++ include/led.h
@@ -50,10 +50,11 @@ class LED {
     }
 };
 
-LED led1(LED_PIN_1, LED_PIN_2);
-LED led2(LED_PIN_3, LED_PIN_4);
-LED led3(LED_PIN_6, LED_PIN_5);
-LED led4(LED_PIN_7, LED_PIN_8);
+LED led1(LED_PIN_1, LED_PIN_2); // INTERNET STATUS
+LED led2(LED_PIN_3, LED_PIN_4); // TANK LEVEL
+LED led3(LED_PIN_6, LED_PIN_5); // OVERFLOW
+LED led4(LED_PIN_7, LED_PIN_8); // PUMP ?
+
 void initLeds();
 void initLeds(){
   led1.begin();
