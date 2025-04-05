225a5b2a3608846305dc17d6b1970b6dcdae9e32
Okko Ojala <okko.ojala@gmail.com>
2025-04-05 17:49:04 +0300

    Remove comments

--- include/dht22_sensor.h
+++ include/dht22_sensor.h
@@ -1,4 +1,3 @@
-//kytkennät
 // - GND
 // + virta vcc 3.3V tai 5V
 // out D2
@@ -12,23 +11,11 @@ DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 
 void getAirTemperatureAndHumidity();
 void getAirTemperatureAndHumidity() {
-  // read humidity
   float humi  = dht_sensor.readHumidity();
-  // read temperature in Celsius
   float temperature_C = dht_sensor.readTemperature();
-  // check whether the reading is successful or not
   if ( isnan(temperature_C) || isnan(humi)) {
     Serial.println("Failed to read from DHT sensor!");
   } else {
-    /*
-    Serial.print("Humidity: ");
-    Serial.print(humi);
-    Serial.print("%");
-    Serial.print("  |  ");
-    Serial.print("Temperature: ");
-    Serial.print(temperature_C);
-    Serial.print("°C  ~  ");
-    */
     airHumidity = humi;
     airTemperature = temperature_C;
   }
