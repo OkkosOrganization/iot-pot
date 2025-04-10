66fdb831c01b035399b36fb6c835d571baf9f198
Okko Ojala <okko.ojala@gmail.com>
2025-04-10 11:11:29 +0300

    Remove comments

--- include/soil_moisture_sensor.h
+++ include/soil_moisture_sensor.h
@@ -2,30 +2,15 @@
 #include <Arduino.h>
 #include "globals.h"
 
-#define AOUT_PIN A2 // Arduino Nano ESP32 pin A2 that connects to AOUT pin of moisture sensor
-//#define THRESHOLD 1500 // CHANGE YOUR THRESHOLD HERE
+#define AOUT_PIN A2 
 
 void initSoilMoistureSensor();
 void getSoilMoistureValue();
 
-//int SoilMoistureValue=0;
-
 void initSoilMoistureSensor(){
-    analogSetAttenuation(ADC_11db);
-
+  analogSetAttenuation(ADC_11db);
 }
 void getSoilMoistureValue(){
-    float value = analogRead(AOUT_PIN); // read the analog value from sensor
-
-  //if (value > THRESHOLD)
-    //Serial.print("The soil is DRY (");
-  //else
-    //Serial.print("The soil is WET (");
-
-  //Serial.print(value);
-  //Serial.println(")");
-
-
-  //delay(500);
+  float value = analogRead(AOUT_PIN);
   soilMoisture=value;
 }
