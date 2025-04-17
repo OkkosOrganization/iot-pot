7d97c2c57b4ee85df667628b723df96e4d789395
Okko Ojala <okko.ojala@gmail.com>
2025-04-17 13:21:36 +0300

    Add interval, add LED control

--- include/water_level_sensor.h
+++ include/water_level_sensor.h
@@ -2,6 +2,7 @@
 // SCL -> A5
 
 #pragma once
+#include <Wire.h>
 #define NO_TOUCH       0xFE
 #define VALUE_THRESHOLD      100
 #define ATTINY1_HIGH_ADDR   0x78
@@ -23,57 +24,73 @@ void getHigh12SectionValue(void)
   memset(high_data, 0, sizeof(high_data));
   Wire.requestFrom(ATTINY1_HIGH_ADDR, 12);
   while (12 != Wire.available());
-  for (int i = 0; i < 12; i++) {
+  for (int i = 0; i < 12; i++)
     high_data[i] = Wire.read();
-  }
 }
 void getLow8SectionValue(void)
 {
   memset(low_data, 0, sizeof(low_data));
   Wire.requestFrom(ATTINY2_LOW_ADDR, 8);
   while (8 != Wire.available());
-  for (int i = 0; i < 8 ; i++) {
+  for (int i = 0; i < 8 ; i++)
     low_data[i] = Wire.read();
-  }
 }
 void getWaterLevel()
 {
-  int sensorvalue_min = 250;
-  int sensorvalue_max = 255;
-  int low_count = 0;
-  int high_count = 0;
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousWaterLevelMillis >= waterLevelInterval) {
+    previousWaterLevelMillis = currentMillis;
 
-  uint32_t touch_val = 0;
-  uint8_t trig_section = 0;
-  low_count = 0;
-  high_count = 0;
-  getLow8SectionValue();
-  getHigh12SectionValue();
+    int sensorvalue_min = 250;
+    int sensorvalue_max = 255;
+    int low_count = 0;
+    int high_count = 0;
 
-  for (int i = 0; i < 8; i++)
-  {
-    if (low_data[i] >= sensorvalue_min && low_data[i] <= sensorvalue_max)
-      low_count++;
-  }
-  for (int i = 0; i < 12; i++)
-  {
-    if (high_data[i] >= sensorvalue_min && high_data[i] <= sensorvalue_max)
-      high_count++;
-  }
-  for (int i = 0 ; i < 8; i++) {
-    if (low_data[i] > VALUE_THRESHOLD) {
-      touch_val |= 1 << i;
+    uint32_t touch_val = 0;
+    uint8_t trig_section = 0;
+    low_count = 0;
+    high_count = 0;
+    getLow8SectionValue();
+    getHigh12SectionValue();
+
+    for (int i = 0; i < 8; i++)
+    {
+      if (low_data[i] >= sensorvalue_min && low_data[i] <= sensorvalue_max)
+        low_count++;
     }
-  }
-  for (int i = 0 ; i < 12; i++) {
-    if (high_data[i] > VALUE_THRESHOLD) {
-      touch_val |= (uint32_t)1 << (8 + i);
+    for (int i = 0; i < 12; i++)
+    {
+      if (high_data[i] >= sensorvalue_min && high_data[i] <= sensorvalue_max)
+        high_count++;
     }
+    for (int i = 0 ; i < 8; i++) {
+      if (low_data[i] > VALUE_THRESHOLD) {
+        touch_val |= 1 << i;
+      }
+    }
+    for (int i = 0 ; i < 12; i++) {
+      if (high_data[i] > VALUE_THRESHOLD) {
+        touch_val |= (uint32_t)1 << (8 + i);
+      }
+    }
+    while (touch_val & 0x01)
+    {
+      trig_section++;
+      touch_val >>= 1;
+    }
+
+    // UPDATE THE GLOBAL VAR
+    waterLevel = trig_section * 5;    
+
+    // SET THE LED STATE
+    if (waterLevel < WATER_LEVEL_LOW)
+      led3.setState(RED);
+    else if (waterLevel >= WATER_LEVEL_LOW && waterLevel < WATER_LEVEL_MEDIUM)
+      led3.setState(YELLOW);
+    else if (waterLevel >= WATER_LEVEL_MEDIUM && waterLevel < WATER_LEVEL_HIGH)
+      led3.setState(GREEN);
+    else
+      led3.setState(RED_BLINK);
   }
-  while (touch_val & 0x01)
-  {
-    trig_section++;
-    touch_val >>= 1;
-  }
-  waterLevel = trig_section * 5;
 }
+
