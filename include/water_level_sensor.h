01a463ea63c08e45685cb941275c8098d02b9489
Okko Ojala <okko.ojala@gmail.com>
2025-06-12 18:33:11 +0300

    Refactor, use class

--- include/water_level_sensor.h
+++ include/water_level_sensor.h
@@ -3,131 +3,110 @@
 
 #pragma once
 #include <Wire.h>
-#define NO_TOUCH       0xFE
-#define VALUE_THRESHOLD      100
+#define NO_TOUCH            0xFE
+#define VALUE_THRESHOLD     100
 #define ATTINY1_HIGH_ADDR   0x78
-#define ATTINY2_LOW_ADDR   0x77
+#define ATTINY2_LOW_ADDR    0x77
 
-void initWaterLevelSensor();
-void getWaterLevel();
-void getHigh12SectionValue(void);
-void getLow8SectionValue(void);
+class WaterLevelSensor {
+  private:
+    unsigned char low_data[8] = {0};
+    unsigned char high_data[12] = {0};
 
-unsigned char low_data[8] = {0};
-unsigned char high_data[12] = {0};
-
-void initWaterLevelSensor(){  
-  Wire.begin();
-}
-void getHigh12SectionValue(void)
-{
-  memset(high_data, 0, sizeof(high_data));
-  Wire.requestFrom(ATTINY1_HIGH_ADDR, 12);
-  
-  unsigned long start = millis();
-  while (Wire.available() < 12) {
-    if (millis() - start > 100) {
-      Serial.println("Timeout waiting for high section data!");
-      return;
+  public:
+    WaterLevelSensor() {
+      Wire.begin();
     }
-  }
-  for (int i = 0; i < 12; i++)
-    high_data[i] = Wire.read();
-
-}
-
-void getLow8SectionValue(void)
-{
-  memset(low_data, 0, sizeof(low_data));
-  Wire.requestFrom(ATTINY2_LOW_ADDR, 8);
-  
-  unsigned long start = millis();
-  while (Wire.available() < 8) {
-    if (millis() - start > 100) {
-      Serial.println("Timeout waiting for low section data!");
-      return;
-    }
-  }
-
-  for (int i = 0; i < 8 ; i++)
-    low_data[i] = Wire.read();
-
-}
-void getWaterLevel()
-{
-  /*
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousWaterLevelMillis >= waterLevelInterval) {
-    previousWaterLevelMillis = currentMillis;
-  }
-  */
-    int sensorvalue_min = 250;
-    int sensorvalue_max = 255;
-    int low_count = 0;
-    int high_count = 0;
-
-    uint32_t touch_val = 0;
-    uint8_t trig_section = 0;
-    low_count = 0;
-    high_count = 0;
-    getLow8SectionValue();
-    getHigh12SectionValue();
-
-    for (int i = 0; i < 8; i++)
+    void getHigh12SectionValue(void)
     {
-      if (low_data[i] >= sensorvalue_min && low_data[i] <= sensorvalue_max)
-        low_count++;
-    }
-    for (int i = 0; i < 12; i++)
-    {
-      if (high_data[i] >= sensorvalue_min && high_data[i] <= sensorvalue_max)
-        high_count++;
-    }
-    for (int i = 0 ; i < 8; i++) {
-      if (low_data[i] > VALUE_THRESHOLD) {
-        touch_val |= 1 << i;
+      memset(high_data, 0, sizeof(high_data));
+      Wire.requestFrom(ATTINY1_HIGH_ADDR, 12);
+      
+      unsigned long start = millis();
+      while (Wire.available() < 12) {
+        if (millis() - start > 100) {
+          Serial.println("Timeout waiting for high section data!");
+          return;
+        }
       }
+      for (int i = 0; i < 12; i++)
+        high_data[i] = Wire.read();
+
     }
-    for (int i = 0 ; i < 12; i++) {
-      if (high_data[i] > VALUE_THRESHOLD) {
-        touch_val |= (uint32_t)1 << (8 + i);
-      }
-    }
-    while (touch_val & 0x01)
+    void getLow8SectionValue(void)
     {
-      trig_section++;
-      touch_val >>= 1;
+      memset(low_data, 0, sizeof(low_data));
+      Wire.requestFrom(ATTINY2_LOW_ADDR, 8);
+      
+      unsigned long start = millis();
+      while (Wire.available() < 8) {
+        if (millis() - start > 100) {
+          Serial.println("Timeout waiting for low section data!");
+          return;
+        }
+      }
+
+      for (int i = 0; i < 8 ; i++)
+        low_data[i] = Wire.read();
+
     }
+    void getWaterLevel(int &waterLevel) {
 
-    /*
-    Serial.print("low_data: ");
-    for (int i = 0; i < 8; i++) {
-      Serial.print(low_data[i]);
-      Serial.print(" ");
-    }
-    Serial.println();
+      int sensorvalue_min = 250;
+      int sensorvalue_max = 255;
+      int low_count = 0;
+      int high_count = 0;
 
-    Serial.print("high_data: ");
-    for (int i = 0; i < 12; i++) {
-      Serial.print(high_data[i]);
-      Serial.print(" ");
-    }
-    Serial.println();
-    */
+      uint32_t touch_val = 0;
+      uint8_t trig_section = 0;
+      low_count = 0;
+      high_count = 0;
+      getLow8SectionValue();
+      getHigh12SectionValue();
 
-    // UPDATE THE GLOBAL VAR
-    waterLevel = trig_section * 5;    
+      for (int i = 0; i < 8; i++)
+      {
+        if (low_data[i] >= sensorvalue_min && low_data[i] <= sensorvalue_max)
+          low_count++;
+      }
+      for (int i = 0; i < 12; i++)
+      {
+        if (high_data[i] >= sensorvalue_min && high_data[i] <= sensorvalue_max)
+          high_count++;
+      }
+      for (int i = 0 ; i < 8; i++) {
+        if (low_data[i] > VALUE_THRESHOLD) {
+          touch_val |= 1 << i;
+        }
+      }
+      for (int i = 0 ; i < 12; i++) {
+        if (high_data[i] > VALUE_THRESHOLD) {
+          touch_val |= (uint32_t)1 << (8 + i);
+        }
+      }
+      while (touch_val & 0x01)
+      {
+        trig_section++;
+        touch_val >>= 1;
+      }
 
-    // SET THE LED STATE
-    if (waterLevel <= WATER_LEVEL_TOO_LOW && led3.getState() != RED_FAST_BLINK)
-      led3.setState(RED_FAST_BLINK);
-    else if (waterLevel > WATER_LEVEL_TOO_LOW && waterLevel < WATER_LEVEL_LOW)
-      led3.setState(RED);      
-    else if (waterLevel >= WATER_LEVEL_LOW && waterLevel < WATER_LEVEL_MEDIUM)
-      led3.setState(YELLOW);
-    else if (waterLevel >= WATER_LEVEL_MEDIUM && waterLevel <= WATER_LEVEL_HIGH)
-      led3.setState(GREEN);
-    else if (waterLevel > WATER_LEVEL_HIGH && led3.getState() != RED_BLINK)
-      led3.setState(RED_BLINK);
-}
+      /*
+      Serial.print("low_data: ");
+      for (int i = 0; i < 8; i++) {
+        Serial.print(low_data[i]);
+        Serial.print(" ");
+      }
+      Serial.println();
 
+      Serial.print("high_data: ");
+      for (int i = 0; i < 12; i++) {
+        Serial.print(high_data[i]);
+        Serial.print(" ");
+      }
+      Serial.println();
+      */
+
+      // UPDATE THE GLOBAL VAR
+      waterLevel = trig_section * 5;
+    };
+};
